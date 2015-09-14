8887388c2e2e49540bef7cdea2c3399e088ef0fb
alpipego <alpipego@gmail.com>
2015-09-14 14:27:19 +0200

    main plugin file

--- postmeta.php
+++ postmeta.php
@@ -5,7 +5,7 @@ namespace QuanDigital\PostMeta;
 /**
  * Plugin Name: Quan Post Meta
  * Plugin URI: http://www.quandigital.com/
- * Description: Adds meta information to posts (with the help of acf)
+ * Description: Adds meta information to posts (requires acf)
  * Version: 1.0.0
  * License: MIT
  */
@@ -17,6 +17,5 @@ use QuanDigital\WpLib\Autoload;
 
 new Autoload(__DIR__, __NAMESPACE__);
 
-// create the plugin, pass the importer as object
-new PostMetaPlugin(__FILE__, $import);
+new Plugin(__FILE__);
     
