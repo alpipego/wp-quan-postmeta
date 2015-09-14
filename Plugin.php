0a8f1e555f5e30b8f15b098d4cd03b5cd0447308
alpipego <alpipego@gmail.com>
2015-09-14 15:30:54 +0200

    removed converted option

--- Plugin.php
+++ Plugin.php
@@ -9,27 +9,15 @@ class Plugin extends Boilerplate
     public function __construct($file)
     {
         parent::__construct($file, [$this, 'convertHeadspace']);
-        // add_action('wp_footer', function() {
-        //     echo '<code><pre>';
-        //         var_dump(get_field('quan_meta_robots'));
-        //     echo '</pre></code>';
-        // });
 
         if (!\is_admin()) {
             $this->outputMeta();
         }
     }
 
-    /**
-     * check if headspace/quan_summary has already been converted
-     * converts other fields to description and updates option
-     * so this does not run every time (i.e. only once or if forced)
-     */
     public function convertHeadspace()
     {
-        if (!get_option('headspace_converted')) {
-            new Convert();
-        }  
+        new Convert();  
     }
 
     function outputMeta()
